a4a6520e273addd96a7796d24c96b104b6ff5fd8
Cntt06 <Admin06@Cntt06s-iMac.local>
2017-07-05 13:41:25 +0700

    first

--- Bai1/ViewController.swift
+++ Bai1/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var inactiveQueue: DispatchQueue!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +19,50 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        //concurrentQueues1(); // Cach 1
+        concurrentQueues2() // Cach 2
+        if let queue = inactiveQueue {
+            queue.activate()
+        }
+    }
+    func concurrentQueues1() {
+        let concurrentQueue = DispatchQueue(label: "thread", qos: .utility, attributes: .concurrent)
+        concurrentQueue.async {
+            for i in 0..<10 {
+                print("🔴", i)
+            }
+        }
+        concurrentQueue.async {
+            for i in 100..<110 {
+                print("🔵", i)
+            }
+        }
+        concurrentQueue.async {
+            for i in 1000..<1010 {
+                print("⚫️", i)
+            }
+        }
+    }
+    func concurrentQueues2() {
+        let concurrentQueue = DispatchQueue(label: "thread1", qos: .userInitiated, attributes: [.concurrent, .initiallyInactive])
+        inactiveQueue = concurrentQueue
+        concurrentQueue.async {
+            for i in 0..<10 {
+                print("🔴", i)
+            }
+        }
+        concurrentQueue.async {
+            for i in 100..<110 {
+                print("🔵", i)
+            }
+        }
+        concurrentQueue.async {
+            for i in 1000..<1010 {
+                print("⚫️", i)
+            }
+        }
+    }
 }
 
